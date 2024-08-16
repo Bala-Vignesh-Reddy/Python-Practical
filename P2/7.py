340d97c57c93641777343110dee96fe97bfd05b1
Bala-Vignesh-Reddy <reddybalavignesh9979@gmail.com>
2024-08-16 18:35:03 +0530

    P2

--- P2/7.py
+++ P2/7.py
@@ -1,3 +1,10 @@
 #write a python program to remove an empty tuple(s) from a list of tuples
 
+lst = [(1, 2, 3), (), ('a', 'b', 'c'), (), (2,)]
+ans = []
 
+for i in lst:
+    if i:
+        ans.append(i)
+
+print(ans)
