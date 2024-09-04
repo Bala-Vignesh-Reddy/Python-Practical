a44a82ff0368dc33052148ff677f3461c5d196ba
Bala-Vignesh-Reddy <reddybalavignesh9979@gmail.com>
2024-09-04 22:48:08 +0530

    updated

--- P3/5.py
+++ P3/5.py
@@ -1,8 +1,21 @@
 #WAP to print the start pattern using nested for loop
 
 for i in range(5):
-    for j in range(5):
-        print("*")
-        i += 1
+    print("*" * i)
+
+for j in range(5, 0, -1):
+    print("*" * j)
 
 
+"""rows = int(input("Enter the number of rows: "))
+
+for i in range(0, rows):
+    for j in range(0, i ):
+        print("*", end=' ')
+    print()
+
+for i in range(rows , 0, -1):
+    for j in range(1, i - 1):
+        print("*", end=' ')
+    print()
+"""
