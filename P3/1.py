f55c311e2695d6b91bd408f6a05e4ee66f19c42e
Bala-Vignesh-Reddy <reddybalavignesh9979@gmail.com>
2024-09-05 11:11:13 +0530

    P3

--- P3/1.py
+++ P3/1.py
@@ -1,9 +1,23 @@
 # take integer input from user until user presses q. Print average and product of all numbers.
 # brute force method there can be more effective method also!!!
-lst = []
-avg = sum1 = 0
-count = 1
 
+# using only one variable
+avg = sum1 = count = 0
+while True:
+    num = input("Enter the number,  Press q to exit!! : ")
+    if num == 'q':
+        break
+    else:
+        sum1 += int(num)
+        count += 1 
+
+print("Sum:", sum1)
+print("Average:", sum1/count)
+
+# using list 
+lst = []
+count = 1
+"""
 while True:
     num = input("Enter the number,  Press q to exit!! : ")
     if num == 'q':
@@ -18,7 +32,6 @@ avg = sum1/count
 for i in lst:
     sum1 += i
     count += 1
-
-
 avg = sum1/count
-print(avg)
+#print(avg)
+"""
