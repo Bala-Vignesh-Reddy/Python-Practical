f6aa46d85e7bb08e02b45a2b0ff1213cf107b102
Balavignesh <108361665+Bala-Vignesh-Reddy@users.noreply.github.com>
2024-09-12 11:26:18 +0530

    P5

---
+++ P5/4.py
@@ -0,0 +1,14 @@
+# Write a function to return the sum of two number. Defined sum as global and local variable and see the difference
+def sum(a, b):
+    local_sum = a + b
+    global global_sum 
+    global_sum = a +b
+    print("Local Sum:", local_sum)
+    return local_sum
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+res = sum(a+b)
+print("Returned value:", res)
+print("Global Sum:", global_sum)
