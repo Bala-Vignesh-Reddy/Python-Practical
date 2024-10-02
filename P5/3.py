b016ec6d11061cdb87514629e417de1e133ea2af
Bala-Vignesh-Reddy <reddybalavignesh9979@gmail.com>
2024-10-02 17:33:44 +0530

    P5

--- P5/3.py
+++ P5/3.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-# Write a function to print default argument when it is not passed to function
-
-def func(name = "Pragnesh"):
-   # var = "Name is " + name 
-    var = f"Name is {name}"
-    return var  
-
-res = func("Bala")
-print(res)
-=======
 # Write a function to print default argument when it is not passed to function
 
 def greet(name="Bala"):
--- P5/4.py
+++ P5/4.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-# Write a function to return the sum of two number. Defined sum as global and local variable and see the difference
-def sum(a, b):
-    local_sum = a + b
-    global global_sum 
-    global_sum = a + b
-    print("Local Sum:", local_sum)
-    return local_sum
-
-a = int(input("Enter first number:"))
-b = int(input("Enter second number:"))
-
-res = sum(a,b)
-print("Returned value:", res)
-print("Global Sum:", local_sum)
-=======
 # Write a function to return the sum of two number. Defined sum as global and local variable and see the difference
 
 #as a global variable 
