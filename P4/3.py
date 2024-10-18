6d50fdb5bf4291f317e85daf3cce25df89e7940e
Balavignesh <108361665+Bala-Vignesh-Reddy@users.noreply.github.com>
2024-10-18 22:04:19 +0530

    Updated

--- P4/3.py
+++ P4/3.py
@@ -1,1 +1,22 @@
 # Draw sine wave using dashed line, range of y axis should be limited in range (-1,1) and draw a line of y=5, text annotate value at x=1.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 100)
+
+y = np.sin(x)
+
+plt.figure(figsize=(10, 5))
+plt.plot(x, y, label='Sine Wave', color='blue')
+
+# Add title and labels
+plt.title('Sine Wave')
+plt.xlabel('x (radians)')
+plt.ylabel('sin(x)')
+plt.axhline(0, color='black',linewidth=0.5, ls='--')
+plt.axvline(0, color='black',linewidth=0.5, ls='--')
+plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+plt.legend()
+
+plt.show()
